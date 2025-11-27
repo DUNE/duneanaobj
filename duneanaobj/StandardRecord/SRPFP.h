f170a17171d491a692d8a73ef586bb90176c717c
Jacob Calcutt <calcuttj@msu.edu>
2025-11-26 18:28:50 -0600

    Adding new fields to SRPFP

--- duneanaobj/StandardRecord/SRPFP.h
+++ duneanaobj/StandardRecord/SRPFP.h
@@ -13,6 +13,41 @@
 
 namespace caf
 {
+  /*
+    This is used to store the results of the EMTrkMichelId Score
+  */
+  struct SRSTEMScore
+  {
+    SRSTEMScore(){};
+    SRSTEMScore(float tscore, float sscore, float escore, float mscore, bool weighted, int plane)
+    : track_score(tscore), shower_score(sscore), empty_score(escore), michel_score(mscore),
+      charge_weighted(weighted), plane_ID(plane) {};
+
+    float track_score{0};
+    float shower_score{0};
+    float empty_score{0};
+    float michel_score{0};
+    bool charge_weighted{false};
+    int plane_ID{-1};
+
+    public:
+      SRSTEMScore& operator/=(double scalar) {
+        track_score /= scalar;
+        shower_score /= scalar;
+        empty_score /= scalar;
+        michel_score /= scalar;
+        return *this; // Return a reference to the modified object
+      }
+
+      SRSTEMScore operator/(double scalar) {
+        track_score /= scalar;
+        shower_score /= scalar;
+        empty_score /= scalar;
+        michel_score /= scalar;
+        return SRSTEMScore(track_score, shower_score, empty_score, michel_score, charge_weighted, plane_ID); // Return a reference to the modified object
+      }
+  };
+
   class SRPFP
   {
     public:
@@ -50,6 +85,16 @@ namespace caf
 
       //Track/Shower BDT score
       float track_score = NaN;
+
+      // EM/Track/Michel ID Scores -- see https://doi.org/10.1140/epjc/s10052-022-10791-2
+      SRSTEMScore cnn_stem_scores;
+
+      int parent = -1;                                ///< Index of parent SRPFP in the same branch, defaults to -1 if no parent
+      std::vector<unsigned int> daughters;             ///< Indices of daughters SRPFP in the same branch
+
+      std::vector<TrueParticleID> truth;              ///< Associated SRTrueParticle(s), if relevant (use SRTruthBranch::Particle() with these IDs to grab them)
+      std::vector<float>   truthOverlap;              ///< Fractional overlap between this track and true particle
+      
   };
 
 }
