1034d09efeb82938c9df85a8e73f490159539650
GianfrancoIngratta <48324147+GianfrancoIngratta@users.noreply.github.com>
2026-03-04 19:24:23 +0000

    Deploying to gh-pages from @ DUNE/duneanaobj@8c845f6a3ca7e61570232bc40479ca81f067d4f1 🚀

--- search/all_10.js
+++ search/all_10.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['q0_0',['q0',['../classcaf_1_1SRTrueInteraction.html#a67c76494a26472fec50d9d16b17f87ad',1,'caf::SRTrueInteraction']]],
   ['q2_1',['Q2',['../classcaf_1_1SRTrueInteraction.html#af066c4ac682a5c1f6d54ad4bbb62b148',1,'caf::SRTrueInteraction']]],
-  ['qual_2',['qual',['../classcaf_1_1SRTrack.html#a29cd75046ca138b552073767a2d60b10',1,'caf::SRTrack']]]
+  ['qual_2',['qual',['../classcaf_1_1SRShower.html#a471da10810e3ab1960d4bb44835595b4',1,'caf::SRShower::qual'],['../classcaf_1_1SRTrack.html#a29cd75046ca138b552073767a2d60b10',1,'caf::SRTrack::qual']]]
 ];
