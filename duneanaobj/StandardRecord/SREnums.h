3cb4cd37833c71e58a03c47609146c23fd361fda
mt82 <matteo.tenti@gmail.com>
2026-03-26 15:16:07 +0000

    remove return type for bool operator

--- duneanaobj/StandardRecord/SREnums.h
+++ duneanaobj/StandardRecord/SREnums.h
@@ -141,7 +141,7 @@ namespace caf
     SRRecoParticleCollectionType type = SRRecoParticleCollectionType::kUnknown; ///< Which of the SRRecoParticle collections in SRRecoParticlesBranch this particle lives in
     int ipart = -1;                                                             ///< Index of SRRecoParticle in the specified SRRecoParticlesBranch collection of the SRInteraction
 
-    inline bool operator bool() const { return !(type == SRRecoParticleCollectionType::kUnknown || ixn < 0 || ipart < 0); }; ///< Returns true if this is a valid ID (i.e. not default/invalid values)
+    inline operator bool() const { return !(type == SRRecoParticleCollectionType::kUnknown || ixn < 0 || ipart < 0); }; ///< Returns true if this is a valid ID (i.e. not default/invalid values)
   };
 
   /// \brief Identifies a reconstructed object by the interaction it belongs to,
@@ -185,7 +185,7 @@ namespace caf
     SRRecoBaseCollectionType type = SRRecoBaseCollectionType::kUnknown;  ///< Which of the low-level reco objects collections this object lives in
     int      irecoobj = -1;                                              ///< Index of SRRecoObjBase in the specified reco objects collection of the SRInteraction 
 
-    inline bool operator bool() const { return !(type == SRRecoBaseCollectionType::kUnknown || ixn < 0 || irecoobj < 0); }; ///< Returns true if this is a valid ID (i.e. not default/invalid values)
+    inline operator bool() const { return !(type == SRRecoBaseCollectionType::kUnknown || ixn < 0 || irecoobj < 0); }; ///< Returns true if this is a valid ID (i.e. not default/invalid values)
   };
 
   /// Which reconstruction toolkit was used to reconstruct this FD event?
