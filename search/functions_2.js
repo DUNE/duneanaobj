3de27867d1a1ce96c3bf6e121f6840f528cdf40e
chenel <chenel@users.noreply.github.com>
2026-08-06 13:40:46 +0000

    Deploying to gh-pages from @ DUNE/duneanaobj@b918d26eb05ae550c3eb1d2edf19a1b5c9faf2eb 🚀

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['is0hc_0',['is0HC',['../classcaf_1_1SRBeamBranch.html#a25f05e02bbe3b6a580fa605b84bda7b8',1,'caf::SRBeamBranch']]],
-  ['isrhc_1',['isRHC',['../classcaf_1_1SRBeamBranch.html#ae9076399a66a68cafaa59ef9837fa8bd',1,'caf::SRBeamBranch']]]
+  ['is_5fantineutrino_5fmode_1',['is_antineutrino_mode',['../classcaf_1_1SRBeamBranch.html#a33bc51c7ec8d49f1b62fbb745a235abb',1,'caf::SRBeamBranch']]],
+  ['is_5fneutrino_5fmode_2',['is_neutrino_mode',['../classcaf_1_1SRBeamBranch.html#aa4c28738640e57b7a6641835c2d22d84',1,'caf::SRBeamBranch']]],
+  ['isrhc_3',['isRHC',['../classcaf_1_1SRBeamBranch.html#ae9076399a66a68cafaa59ef9837fa8bd',1,'caf::SRBeamBranch']]]
 ];
